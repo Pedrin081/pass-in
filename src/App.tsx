b89e9eecc814886f383d7ea22c465dd326727d9d
Pedro Victor <pedro.vnascimento@outlook.com>
2024-04-03 15:12:50 -0300

    inicio projeto

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  
+  return (
+    <h1 className="bg-orange-400">Hello Wolrd</h1>
+  )
+
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
