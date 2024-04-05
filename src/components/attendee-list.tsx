de8b9604b6da8b3341cdd783cd095f0ac2f9181e
Pedrin081 <pedro.vnascimento@outlook.com>
2024-04-04 21:44:57 -0300

    adicionando novas coisas

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,6 +1,9 @@
 import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from "lucide-react"
 import { IconButton } from "./icon-button"
 import { Table } from "./table/table"
+import { TableHeader } from "./table/table-header"
+import { TableCell } from "./table/table-td"
+import { TableRows } from "./table/table-rows"
 
 export function Attendeelist() {
 
@@ -17,47 +20,47 @@ export function Attendeelist() {
             <Table>
                 <thead>
                     <tr className="border border-white/10">
-                        <th style={{ width: 48 }} className="py-3 px-4 text-sm font-semibold text-left">
+                        <TableHeader style={{ width: 48 }} >
                             <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10 " />
-                        </th>
-                        <th className="py-3 px-4 text-sm font-semibold text-left">código</th>
-                        <th className="py-3 px-4 text-sm font-semibold text-left">participante</th>
-                        <th className="py-3 px-4 text-sm font-semibold text-left">Data de inscrição</th>
-                        <th className="py-3 px-4 text-sm font-semibold text-left">Data do check-in</th>
-                        <th style={{ width: 64 }} className="py-3 px-4 text-sm font-semibold text-left"></th>
+                        </TableHeader>
+                        <TableHeader >código</TableHeader>
+                        <TableHeader >participante</TableHeader>
+                        <TableHeader >Data de inscrição</TableHeader>
+                        <TableHeader >Data do check-in</TableHeader>
+                        <TableHeader style={{ width: 64 }} ></TableHeader>
                     </tr>
                 </thead>
                 <tbody>
                     {Array.from({ length: 8 }).map((_, i) => {
                         return (
-                            <tr key={i} className="border-b border-white/10 hover: bg-white/10 ">
+                            <TableRows key={i} className="border-b border-white/10 hover:bg-white/10 ">
 
-                                <td className="py-3 px-4 text-sm font-semibold">   <input type="checkbox" className="size-4 bg-black    /20 rounded border-white/10 " /> </td>
+                                <TableCell className="py-3 px-4 text-sm font-semibold">   <input type="checkbox" className="size-4 bg-black    /20 rounded border-white/10 " /> </TableCell>
 
-                                <td className="py-3 px-4 text-sm text-zinc-300">2323</td>
-                                <td className="py-3 px-4 text-sm text-zinc-300">
+                                <TableCell>2323</TableCell>
+                                <TableCell>
                                     <div className="flex flex-col gap-1">
                                         <span className="font-semibold text-white ">Pedro victor</span>
                                         <span>Email@2332</span>
                                     </div>
-                                </td>
+                                </TableCell>
 
-                                <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
-                                <td className="py-3 px-4 text-sm text-zinc-300">2 dias atrás</td>
-                                <td className="py-3 px-4 text-sm text-zinc-300">
+                                <TableCell >7 dias atrás</TableCell>
+                                <TableCell >2 dias atrás</TableCell>
+                                <TableCell >
                                     <IconButton transparent={true}>
                                         <MoreHorizontal className="size-4" />
                                     </IconButton>
-                                </td>
-                            </tr>
+                                </TableCell>
+                            </TableRows>
                         )
                     })}
                 </tbody>
                 <tfoot>
                     <tr >
-                        <td colSpan={3} className="py-3 px-4 text-sm text-zinc-300">
+                        <TableCell colSpan={3} className="py-3 px-4 text-sm text-zinc-300">
                             Mostrando 10 de 200 itens
-                        </td>
+                        </TableCell>
                         <td colSpan={3} className="py-3 px-4 text-sm text-zinc-300 text-right">
 
                             <div className="inline-flex items-center gap-8">
